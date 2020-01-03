c0d391bfec24d7e580272786c531201827c63a33
ThiagoCaiado <thiagocaiadodev@gmail.com.br>
2020-01-02 23:29:50 -0300

    master

--- app/src/main/java/com/thiago/marvelapp/di/remote/RemoteModule.kt
+++ app/src/main/java/com/thiago/marvelapp/di/remote/RemoteModule.kt
@@ -2,12 +2,17 @@ package com.thiago.marvelapp.di.remote
 
 import com.thiago.remote.RemoteProject
 import com.thiago.remote.endpoint.ApiImplementation
+import com.thiago.remote.service.ServiceApi
 import org.koin.dsl.module
 
 val remoteModule = module{
 
+    single<ServiceApi>{
+        ServiceApi()
+    }
+
     single<ApiImplementation>{
-        ApiImplementation()
+        ApiImplementation(get())
     }
 
     single<RemoteProject>{
--- remote/src/main/java/com/thiago/remote/endpoint/ApiImplementation.kt
+++ remote/src/main/java/com/thiago/remote/endpoint/ApiImplementation.kt
@@ -2,13 +2,7 @@ package com.thiago.remote.endpoint
 
 import com.thiago.remote.service.ServiceApi
 
-class ApiImplementation {
-
-    private val serviceApi: ServiceApi
-
-    init{
-        serviceApi = ServiceApi()
-    }
+class ApiImplementation(private val serviceApi: ServiceApi) {
 
     val SERVICE: ApiEndpoint by lazy {
         serviceApi.getDataService().create(ApiEndpoint::class.java)
